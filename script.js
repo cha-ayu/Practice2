6667420a39e15c9e2af4770bde8ba547300ec6a3
cha-ayu <pickles.the.frog.yu@gmail.com>
2019-12-07 16:25:23 +0900

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function() {
+
+// 新規登録モーダル
+
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();  
+  });
+
+  $('#close-modal').click(function() {
+    $('#signup-modal').fadeOut();  
+  });
+
+  
+});
